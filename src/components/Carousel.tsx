e7275bf2aa3bb62603460856e7799e3c0c4e68c4
Nazar Bakay <bakayn484@gmail.com>
2025-07-23 23:18:19 +0300

    add solution

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+  infinite,
+}) => {
+  const [valueX, setValueX] = useState(0);
+  const [countStepPrev, setCountStepPrev] = useState(-1);
+  const [countStepNext, setCountStepNext] = useState(
+    Math.floor(images.length / step - 1),
+  );
+  const correctStepPrev = countStepPrev !== 0 ? step : images.length % step;
+  const correctStepNext = countStepNext !== 0 ? step : images.length % step;
+
+  return (
+    <div
+      className="Carousel"
+      style={
+        {
+          '--x': `${valueX}px`,
+          '--size-image': `${itemWidth}px`,
+          '--frame-size': frameSize,
+          '--animation-duration': `${animationDuration}ms`,
+        } as React.CSSProperties
+      }
+    >
+      <ul className="Carousel__list">
+        {images.map((img, index) => (
+          <li key={index}>
+            <img src={img} alt={`${index + 1}`} />
+          </li>
+        ))}
+      </ul>
+
+      <button
+        type="button"
+        onClick={() =>
+          setValueX(prev => {
+            {
+              setCountStepPrev(countStepPrev - 1);
+              setCountStepNext(countStepNext + 1);
+              if (infinite) {
+                return prev < 0
+                  ? prev + itemWidth * correctStepPrev
+                  : -itemWidth * (images.length - frameSize);
+              }
+
+              return prev < 0 ? prev + itemWidth * correctStepPrev : prev;
+            }
+          })
+        }
+      >
+        Prev
+      </button>
+      <button
+        type="button"
+        data-cy="next"
+        onClick={() =>
+          setValueX(prev => {
+            setCountStepPrev(countStepPrev + 1);
+            setCountStepNext(countStepNext - 1);
+            if (infinite) {
+              return prev > -itemWidth * (images.length - frameSize)
+                ? prev - itemWidth * correctStepNext
+                : 0;
+            }
+
+            return prev > -itemWidth * (images.length - frameSize)
+              ? prev - itemWidth * correctStepNext
+              : prev;
+          })
+        }
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
